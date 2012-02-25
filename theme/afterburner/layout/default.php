c4d8fd0e3fe919981038340f85be47e3ac9816bb
Nadav Kavalerchik <nadavkav@gmail.com>
2012-02-26 01:18:49 +0200

    theme/afterburner - (major) Navigation and Settings blocks displayed on the right column in RTL mode

--- theme/afterburner/layout/default.php
+++ theme/afterburner/layout/default.php
@@ -18,9 +18,17 @@ $haslogo = (!empty($PAGE->theme->settings->logo));
 
 $bodyclasses = array();
 if ($showsidepre && !$showsidepost) {
-    $bodyclasses[] = 'side-pre-only';
+      if (!right_to_left()) {
+        $bodyclasses[] = 'side-pre-only';
+    } else {
+        $bodyclasses[] = 'side-post-only';
+    }
 } else if ($showsidepost && !$showsidepre) {
-    $bodyclasses[] = 'side-post-only';
+    if (!right_to_left()) {
+        $bodyclasses[] = 'side-post-only';
+    } else {
+        $bodyclasses[] = 'side-pre-only';
+    }
 } else if (!$showsidepost && !$showsidepre) {
     $bodyclasses[] = 'content-only';
 }
@@ -90,18 +98,26 @@ echo $OUTPUT->doctype() ?>
                  </div>
                </div>
 
-                <?php if ($hassidepre) { ?>
+                <?php if ($hassidepre OR (right_to_left() AND $hassidepost)) { ?>
                 <div id="region-pre" class="block-region">
-                   <div class="region-content">
-                        <?php echo $OUTPUT->blocks_for_region('side-pre') ?>
-                   </div>
+                    <div class="region-content">
+                        <?php if (!right_to_left()) {
+                                echo $OUTPUT->blocks_for_region('side-pre');
+                              } elseif ($hassidepost) {
+                                echo $OUTPUT->blocks_for_region('side-post');
+                              } ?>
+                    </div>
                 </div>
                 <?php } ?>
 
-                <?php if ($hassidepost) { ?>
+                <?php if ($hassidepost OR (right_to_left() AND $hassidepre)) { ?>
                 <div id="region-post" class="block-region">
-                   <div class="region-content">
-                        <?php echo $OUTPUT->blocks_for_region('side-post') ?>
+                    <div class="region-content">
+                        <?php if (!right_to_left()) {
+                                echo $OUTPUT->blocks_for_region('side-post');
+                              } elseif ($hassidepre) {
+                                echo $OUTPUT->blocks_for_region('side-pre');
+                              } ?>
                    </div>
                 </div>
                 <?php } ?>
