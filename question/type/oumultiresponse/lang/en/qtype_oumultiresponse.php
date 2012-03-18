d12b777ba847721fe19cea8037141fae79bc8ea0
Nadav Kavalerchik <nadavkav@gmail.com>
2012-03-18 22:20:00 +0200

    question/type/oumultiresponse (new) Initial Hebrew translation

---
+++ question/type/oumultiresponse/lang/en/qtype_oumultiresponse.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * OU multiple response question type language strings.
+ *
+ * @package    qtype_oumultiresponse
+ * @copyright  2008 The Open University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['addingoumultiresponse'] = 'Adding an OU multiple response question';
+$string['choices'] = 'Available choices';
+$string['correctanswer'] = 'Correct';
+$string['editingoumultiresponse'] = 'Editing an OU multiple response question';
+$string['notenoughcorrectanswers'] = 'You must select at least one correct choice';
+$string['oumultiresponse'] = 'OU multiple response';
+$string['oumultiresponse_help'] = 'A multiple-choice, multiple-response question type with particular scoring rules.';
+$string['oumultiresponse_link'] = 'question/type/oumultiresponse';
+$string['oumultiresponsesummary'] = 'A multiple-choice, multiple-response question type with particular scoring rules.';
+$string['pluginname'] = 'OU multiple response';
+$string['pluginname_help'] = 'A multiple-choice, multiple-response question type with particular scoring rules.';
+$string['pluginname_link'] = 'question/type/oumultiresponse';
+$string['pluginnameadding'] = 'Adding an OU multiple response question';
+$string['pluginnameediting'] = 'Editing an OU multiple response question';
+$string['pluginnamesummary'] = 'A multiple-choice, multiple-response question type with particular scoring rules.';
+$string['toomanyoptions'] ='You have selected too many options.';
+$string['showeachanswerfeedback'] = 'Show the feedback for the selected responses.';
---
+++ question/type/oumultiresponse/lang/he/qtype_oumultiresponse.php
@@ -0,0 +1,42 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local language pack from http://moodlemagic.info/moodle
+ *
+ * @package    qtype
+ * @subpackage oumultiresponse
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addingoumultiresponse'] = 'הוספת שאלת רב בחירה OU';
+$string['choices'] = 'אפשרויות זמינות';
+$string['correctanswer'] = 'תשובה נכונה';
+$string['editingoumultiresponse'] = 'הוספת שאלת רב בחירה OU';
+$string['notenoughcorrectanswers'] = 'עליכם לבחור לפחות תשובה אחת נכונה';
+$string['oumultiresponse'] = 'OU תשובות מרובות';
+$string['oumultiresponsesummary'] = 'לסוג שאלת רב בחירה, שיטת ניקוד ייחודית משלה.';
+$string['oumultiresponse_help'] = 'לסוג שאלת רב בחירה, שיטת ניקוד ייחודית משלה.';
+$string['pluginname'] = 'OU תשובות מרובות';
+$string['pluginnameadding'] = 'הוספת שאלת רב בחירה OU';
+$string['pluginnameediting'] = 'עריכת שאלת רב בחירה OU';
+$string['pluginnamesummary'] = 'לסוג שאלת רב בחירה, שיטת ניקוד ייחודית משלה.';
+$string['pluginname_help'] = 'לסוג שאלת רב בחירה, שיטת ניקוד ייחודית משלה.';
+$string['showeachanswerfeedback'] = 'תצוגת המשוב על התשובות הנבחרות.';
+$string['toomanyoptions'] = 'בחרתם יותר מדי אפשרויות';
