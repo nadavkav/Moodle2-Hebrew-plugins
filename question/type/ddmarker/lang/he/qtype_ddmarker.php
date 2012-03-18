f5fed125b08e302ffb7e459d18ad1688affe9d23
Nadav Kavalerchik <nadavkav@gmail.com>
2012-03-18 22:18:12 +0200

    question/type/ddmarker (new) Initial Hebrew Translation

---
+++ question/type/ddmarker/lang/he/qtype_ddmarker.php
@@ -0,0 +1,88 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local language pack from http://moodlemagic.info/moodle
+ *
+ * @package    qtype
+ * @subpackage ddmarker
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addmoreitems'] = 'שדות ריקים {לא עוד} עבור סמנים נוספים';
+$string['answer'] = 'תשובה';
+$string['bgimage'] = 'תמונת רקע';
+$string['coords'] = 'צירים';
+$string['correctansweris'] = 'התשובה הנכונה היא:  {$a}';
+$string['draggableimage'] = 'תמונה נגררת';
+$string['draggableitem'] = 'פריט נגרר';
+$string['draggableitemheader'] = 'פריט נגרר  {$a}';
+$string['draggableitemtype'] = 'סוג';
+$string['draggableword'] = 'כיתוב נגרר';
+$string['dropbackground'] = 'גרירת סמנים על גבי תמונת רקע';
+$string['dropzone'] = 'אזור שחרור {$a}';
+$string['dropzoneheader'] = 'אזורי שחרור';
+$string['followingarewrong'] = 'הסמנים הבאים הוצבו באזור הלא נכון: {$}.';
+$string['followingarewrongandhighlighted'] = 'הסמנים הבאים הונחו בצורה לא נכונה: {$}.  סמן (ים) מודגשים, מוצגים כעת במיקום הנכון.  <br /> הקישו על הסמן כדי להדגיש את השטח המותר.';
+$string['formerror_nobgimage'] = 'עליכם לבחור תמונה, על מנת שתשמש כרקע עבור הגרירה והשחרור של הסמנים על גביה.';
+$string['formerror_noitemselected'] = 'ציינתם אזור שחרור, אך עדיין לא בחרתם בסמן שיש לגרור לאזור זה.';
+$string['formerror_nosemicolons'] = 'אין פסיקים במחרוזת מערכת הצירים שלכם. מערכת הצירים שלכם עבור {$ a-> הצורה} צריכה להיות מצוינת כ-
+ {$ a-> מחרוזת ציר}.';
+$string['formerror_onlyusewholepositivenumbers'] = 'אנא השתמשו רק במספרים חיוביים שלמים כדי לציין בבערכת צירים x,y     ו / או רוחב וגובה של צורות. מערכת הצירים  עבור{$ a-> צורה} צריכה להיות מצוינת כ- 
+{$ a-> מחרוזת ציר}.';
+$string['formerror_polygonmusthaveatleastthreepoints'] = 'עבור צורת מצולע עליכם לציין לפחות 3 נקודות. מערכת הצירים עבור
+ {$ a-> צורה} צריכה להיות מצוינת כ - 
+{$ a-> מחרוזת ציר}.';
+$string['formerror_shapeoutsideboundsofbgimage'] = 'הצורה שהגדרתם חורגת מגבולות תמונת הרקע.';
+$string['formerror_unrecognisedwidthheightpart'] = 'מידות הרוחב והגובה שציינתם אינם מוכרים למערכת. מערכת הצירים עבור 
+{$ a-> צורה} צריכות להכתב כ - 
+{$ a-> מחרוזת ציר}.';
+$string['formerror_unrecognisedxypart'] = 'מערכת הצירים x,y שציינתם אינם מוכרים למערכת. מערכת הצירים עבור {$ a-> צורה} צריכות להכתב כ - 
+{$ a-> מחרוזת}.';
+$string['infinite'] = 'עד אינסוף';
+$string['marker'] = 'סמן';
+$string['markers'] = 'סמנים';
+$string['marker_n'] = 'סמן {לא}';
+$string['nolabel'] = 'אין תווית מסוג כיתוב';
+$string['pleasedragatleastonemarker'] = 'תשובתכם אינה מלאה. עליכם למקם לפחות סמן אחד על התמונה';
+$string['pluginname'] = 'סמניי גרירה ושחרור';
+$string['pluginnameadding'] = 'הוספת סמניי גרירה ושחרור';
+$string['pluginnameediting'] = 'עריכת סמניי גרירה ושחרור';
+$string['pluginnamesummary'] = 'סמנים נגררים ומושלכים על גבי תמונת רקע.';
+$string['pluginname_help'] = 'ראשית בחרו קובץ מסוג תמונת רקע, ולאחר מכן הזינו תוויות כיתוב עבור הסמנים. בהמשך, הגדירו את אזוריי השחרור שעל גבי התמונת רקע, שאליהם  אמורים להיגרר הסמנים שנבחרו.';
+$string['previewarea'] = 'תצוגת אזור-';
+$string['previewareaheader'] = 'תצוגה';
+$string['previewareamessage'] = 'ראשית בחרו קובץ מסוג תמונת רקע, ולאחר מכן הזינו תוויות כיתוב עבור הסמנים. בהמשך, הגדירו את אזוריי השחרור שעל גבי התמונת רקע, שאליהם  אמורים להיגרר הסמנים שנבחרו.';
+$string['refresh'] = 'רענון תצוגה';
+$string['shape'] = 'צורה';
+$string['shape_circle'] = 'עיגול';
+$string['shape_circle_coords'] = 'x, y)  r , x, y הם מערכת הצירים של מרכז המעגל,  וr הוא הרדיוס)';
+$string['shape_circle_lowercase'] = 'עיגול';
+$string['shape_polygon'] = 'מצולע';
+$string['shape_polygon_coords'] = 'X1, Y1, X2, Y2, x3, y3, X4, y4 .... (כאשר x1, y1 הם מערכת צירים x, y  של קודקוד הראשון, X2, Y2 הם מערכת צירים x, y  של השני, וכו. אין צורך לחזור על נקודות הציון של הקודקוד הראשון כדי  לסגור את המצולע)';
+$string['shape_polygon_lowercase'] = 'מצולע';
+$string['shape_rectangle'] = 'מלבן';
+$string['shape_rectangle_coords'] = 'x, y, w, h (כאשר x, y הם  מערכת צירים xy של הפינה השמאלית העליונה של המלבן, w ו-h הם הרוחב והגובה של מלבן)';
+$string['shape_rectangle_lowercase'] = 'מלבן';
+$string['showmisplaced'] = 'הדגשת אזורי השחרור  שאינם מודגשים בסמן השחרור הנכון';
+$string['shuffleimages'] = 'ערבוב פרטיי שחרור בכל פעם שנשאלת שאלה';
+$string['stateincorrectlyplaced'] = 'ציינו כאשר סמנים הונחו בצורה לא נכונה';
+$string['summariseplace'] = '{$a->לא} .  {$a->כיתוב}';
+$string['summariseplaceno'] = 'אזור שחרור {$a}';
+$string['ytop'] = 'למעלה';
