bbc1cabe11c2cfda2f2162334df4bf32a53eac24
Nadav Kavalerchik <nadavkav@gmail.com>
2012-03-18 22:17:25 +0200

    question/type/ddimageortext (new) Initial Hebrew translation

---
+++ question/type/ddimageortext/lang/he/qtype_ddimageortext.php
@@ -0,0 +1,68 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local language pack from http://moodlemagic.info/moodle
+ *
+ * @package    qtype
+ * @subpackage ddimageortext
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addmoredropzones'] = 'שדה ריק עבור עוד {לא עוד} אזורי  שחרור';
+$string['addmoreimages'] = 'שדה ריק עבור עוד {לא עוד} פריטים נגררים';
+$string['answer'] = 'תשובה';
+$string['bgimage'] = 'תמונת רקע';
+$string['correctansweris'] = 'התשובה הנכונה היא: {$a}';
+$string['draggableimage'] = 'תמונה נגררת';
+$string['draggableitem'] = 'פריט נגרר';
+$string['draggableitemheader'] = 'פריט נגרר {$a}';
+$string['draggableitemtype'] = 'סוג';
+$string['draggableword'] = 'כיתוב נגרר';
+$string['dropbackground'] = 'גרירת סמנים על תמונת רקע';
+$string['dropzone'] = 'אזור שחרור {$a}';
+$string['dropzoneheader'] = 'אזור  שחרור';
+$string['formerror_multipledraginstance'] = 'בחרתם בתמונה זו {$} כבחירה הנכונה,יותר מפעם אחת  עבור אזור השחרור, אבל היא לא סומנה כבחירה סופית עבור פריט נגרר.';
+$string['formerror_multipledraginstance2'] = 'בחרתם בתמונה זו כבחירה הנכונה,יותר מפעם אחת  עבור אזור השחרור, אבל היא לא סומנה כבחירה סופית עבור פריט נגרר.';
+$string['formerror_nobgimage'] = 'עליכם לבחור תמונה שתשמש כרקע עבור גרירה והורדה על גביה.';
+$string['formerror_nofile'] = 'עליכם להעלות או לבחור קובץ ולהשתמש בו כאן.';
+$string['formerror_nofile3'] = 'עליכם לבחור בקובץ תמונה, או למחוק את התווית המצורפת ולבטל את תיבת הסימון האינסופי.';
+$string['formerror_noimageselected'] = 'עליכם לבחור פריט נגרר המתאים לאזור שחרור זה';
+$string['formerror_noxleft'] = 'עליכם לספק ערך עבור ציריי y ,בפינה השמאלית העליונה של אזור שחרור זה. אתם יכולים לגרור ולשחרר את אזור השחרור למעלה כדי לקבוע את מערכות הצירים, או להזין אותם כאן באופן ידני .';
+$string['formerror_noytop'] = 'עליכם לספק ערך עבור ציריי y ,בפינה השמאלית העליונה של אזור שחרור זה. אתם יכולים לגרור ולשחרר את אזור השחרור למעלה כדי לקבוע את מערכות הצירים, או להזין אותם כאן באופן ידני .';
+$string['infinite'] = 'עד אינסוף';
+$string['label'] = 'כיתוב';
+$string['nolabel'] = 'ללא תווית כיתוב';
+$string['pleasedraganimagetoeachdropregion'] = 'תשובתכם אינה מלאה, אנא גררו פריט לכל אחד ואחד מאזורי השחרור .';
+$string['pluginname'] = '"גרירה ושחרור על גבי תמונה"';
+$string['pluginnameadding'] = 'הוספת "גרירה ושחרור על גבי תמונה"';
+$string['pluginnameediting'] = 'עריכת "גרירה ושחרור על גבי תמונה"';
+$string['pluginnamesummary'] = 'תמונות או תוויות כיתוב, נגררות ומושלכות לתוך אזורי שחרור, על גבי תמונת רקע.';
+$string['pluginname_help'] = 'ראשית בחרו קובץ מסוג תמונת רקע ולאחר מכן, בחרו תמונות נגררות או הזינו כיתוב. הגדירו את אזוריי השחרור שעל גבי התמונת רקע, שאליהם  אמורים להיגרר הפריטים שנבחרו.';
+$string['previewarea'] = 'תצוגה מקדימה של האזור -';
+$string['previewareaheader'] = 'תצוגה מקדימה';
+$string['previewareamessage'] = 'ראשית בחרו קובץ מסוג תמונת רקע, ולאחר מכן בחרו תמונות נגררות או הזינו כיתוב נגרר. בשלב הבא, בחרו בפריט הנגרר עבור כל "אזור שחרור", וגררו את הפריט  למקום המתאים (למקום שעל התלמיד לגרור אותו אליו).';
+$string['refresh'] = 'רענון תצוגה מקדימה';
+$string['shuffleimages'] = 'ערבוב פריטים נגררים בכל פעם שנשאלת שאלה';
+$string['summarisechoice'] = '{$a->לא}. {$a->כיתוב}';
+$string['summarisechoiceno'] = 'פריט {$a}';
+$string['summariseplace'] = '{$a->לא}. {$a->כיתוב}';
+$string['summariseplaceno'] = 'אזור שחרור  {$a}';
+$string['xleft'] = 'שמאלה';
+$string['ytop'] = 'למעלה';
