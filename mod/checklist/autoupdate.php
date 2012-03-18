35233a671caa7ce1870a767b19e1cfb74b2eca8d
Nadav Kavalerchik <nadavkav@gmail.com>
2012-03-18 22:02:19 +0200

    mod/checklist (new) Initial Hebrew translation and minor RTL CSS fixes

---
+++ mod/checklist/autoupdate.php
@@ -0,0 +1,249 @@
+<?php
+
+// This file is part of the Checklist plugin for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+defined('MOODLE_INTERNAL') || die();
+
+$CFG->checklist_autoupdate_use_cron = true;
+
+function checklist_autoupdate($courseid, $module, $action, $cmid, $userid, $url, $checklists=null) {
+    global $CFG, $DB;
+
+    if ($userid == 0) {
+        return 0;
+    }
+
+    if ($module == 'course') {
+        return 0;
+    }
+    if ($module == 'user') {
+        return 0;
+    }
+    if ($module == 'role') {
+        return 0;
+    }
+    if ($module == 'notes') {
+        return 0;
+    }
+    if ($module == 'calendar') {
+        return 0;
+    }
+    if ($module == 'message') {
+        return 0;
+    }
+    if ($module == 'admin/mnet') {
+        return 0;
+    }
+    if ($module == 'blog') {
+        return 0;
+    }
+    if ($module == 'tag') {
+        return 0;
+    }
+    if ($module == 'blocks/tag_youtube') {
+        return 0;
+    }
+    if ($module == 'login') {
+        return 0;
+    }
+    if ($module == 'library') {
+        return 0;
+    }
+    if ($module == 'upload') {
+        return 0;
+    }
+
+    if (
+        (($module == 'survey') && ($action == 'submit'))
+        || (($module == 'quiz') && ($action == 'close attempt'))
+        || (($module == 'forum') && (($action == 'add post')||($action == 'add discussion')))
+        || (($module == 'resource') && ($action == 'view'))
+        || (($module == 'page') && ($action == 'view'))
+        || (($module == 'hotpot') && ($action == 'submit'))
+        || (($module == 'wiki') && ($action == 'edit'))
+        || (($module == 'checklist') && ($action == 'complete'))
+        || (($module == 'choice') && ($action == 'choose'))
+        || (($module == 'lams') && ($action == 'view'))
+        || (($module == 'scorm') && ($action == 'view'))
+        || (($module == 'assignment') && ($action == 'upload'))
+        || (($module == 'journal') && ($action == 'add entry'))
+        || (($module == 'lesson') && ($action == 'end'))
+        || (($module == 'realtimequiz') && ($action == 'submit'))
+        || (($module == 'workshop') && ($action == 'submit'))
+        || (($module == 'glossary') && ($action == 'add entry'))
+        || (($module == 'data') && ($action == 'add'))
+        || (($module == 'chat') && ($action == 'talk'))
+        || (($module == 'feedback') && ($action == 'submit'))
+        ) {
+
+        if ($cmid == 0) {
+            $matches = array();
+            if (!preg_match('/id=(\d+)/i', $url, $matches)) {
+                return 0;
+            }
+            $cmid = $matches[1];
+        }
+
+        if (!$checklists) {
+            $checklists = $DB->get_records_select('checklist',
+                                                  'course = ? AND autoupdate > 0',
+                                                  array($courseid));
+
+            if (empty($checklists)) {
+                return 0;
+                // No checklists in this course that are auto-updating
+            }
+        }
+
+        if (isset($CFG->enablecompletion) && $CFG->enablecompletion) {
+            // Completion is enabled on this site, so we need to check if this module
+            // can do completion (and then wait for that to indicate the module is complete)
+            $coursecompletion = $DB->get_field('course',
+                                               'enablecompletion',
+                                               array('id'=>$courseid));
+            if ($coursecompletion) {
+                $cmcompletion = $DB->get_field('course_modules',
+                                               'completion',
+                                               array('id'=>$cmid));
+                if ($cmcompletion) {
+                    return 0;
+                }
+            }
+        }
+
+        // Find all checklist_item records which are related to these $checklists which have a moduleid matching $module
+        // and any information about checks they might have
+        list($csql, $cparams) = $DB->get_in_or_equal(array_keys($checklists));
+        $params = array_merge(array($userid, $cmid), $cparams);
+
+        $sql = "SELECT i.id itemid, c.id checkid, c.usertimestamp FROM {checklist_item} i ";
+        $sql .= "LEFT JOIN {checklist_check} c ON (c.item = i.id AND c.userid = ?) ";
+        $sql .= "WHERE i.moduleid = ? AND i.checklist $csql AND i.itemoptional < 2";
+        $items = $DB->get_records_sql($sql, $params);
+        // itemoptional - 0: required; 1: optional; 2: heading;
+        // not loading defines from mod/checklist/locallib.php to reduce overhead
+        if (empty($items)) {
+            return 0;
+        }
+
+        $updatecount = 0;
+        foreach ($items as $item) {
+            if ($item->checkid) {
+                if ($item->usertimestamp) {
+                    continue;
+                }
+                $check = new stdClass;
+                $check->id = $item->checkid;
+                $check->usertimestamp = time();
+                $DB->update_record('checklist_check', $check);
+                $updatecount++;
+            } else {
+                $check = new stdClass;
+                $check->item = $item->itemid;
+                $check->userid = $userid;
+                $check->usertimestamp = time();
+                $check->teachertimestamp = 0;
+                $check->teachermark = 0;
+                // CHECKLIST_TEACHERMARK_UNDECIDED - not loading from mod/checklist/lib.php to reduce overhead
+
+                $check->id = $DB->insert_record('checklist_check', $check);
+                $updatecount++;
+            }
+        }
+        if ($updatecount) {
+            require_once($CFG->dirroot.'/mod/checklist/lib.php');
+            foreach ($checklists as $checklist) {
+                checklist_update_grades($checklist, $userid);
+            }
+            return $updatecount;
+        }
+    }
+
+    return 0;
+}
+
+function checklist_completion_autoupdate($cmid, $userid, $newstate) {
+    global $DB, $CFG, $USER;
+
+    if ($userid == 0) {
+        $userid = $USER->id;
+    }
+    $sql = "SELECT i.id itemid, c.id checkid, c.usertimestamp, i.checklist FROM {checklist_item} i ";
+    $sql .= "JOIN {checklist} cl ON i.checklist = cl.id ";
+    $sql .= "LEFT JOIN {checklist_check} c ON (c.item = i.id AND c.userid = ?) ";
+    $sql .= "WHERE cl.autoupdate > 0 AND i.moduleid = ? AND i.itemoptional < 2 ";
+    $items = $DB->get_records_sql($sql, array($userid, $cmid));
+    // itemoptional - 0: required; 1: optional; 2: heading;
+    // not loading defines from mod/checklist/locallib.php to reduce overhead
+    if (empty($items)) {
+        return 0;
+    }
+
+    $newstate = ($newstate == COMPLETION_COMPLETE || $newstate == COMPLETION_COMPLETE_PASS); // Not complete if failed
+    $updatecount = 0;
+    $updatechecklists = array();
+    foreach ($items as $item) {
+        if ($item->checkid) {
+            if ($newstate) {
+                if ($item->usertimestamp) {
+                    continue;
+                }
+                $check = new stdClass;
+                $check->id = $item->checkid;
+                $check->usertimestamp = time();
+                $DB->update_record('checklist_check', $check);
+                $updatechecklists[] = $item->checklist;
+                $updatecount++;
+            } else {
+                if (!$item->usertimestamp) {
+                    continue;
+                }
+                $check = new stdClass;
+                $check->id = $item->checkid;
+                $check->usertimestamp = 0;
+                $DB->update_record('checklist_check', $check);
+                $updatechecklists[] = $item->checklist;
+                $updatecount++;
+            }
+        } else {
+            if (!$newstate) {
+                continue;
+            }
+            $check = new stdClass;
+            $check->item = $item->itemid;
+            $check->userid = $userid;
+            $check->usertimestamp = time();
+            $check->teachertimestamp = 0;
+            $check->teachermark = 0;
+            // CHECKLIST_TEACHERMARK_UNDECIDED - not loading from mod/checklist/lib.php to reduce overhead
+
+            $check->id = $DB->insert_record('checklist_check', $check);
+            $updatechecklists[] = $item->checklist;
+            $updatecount++;
+        }
+    }
+    if (!empty($updatechecklists)) {
+        $updatechecklists = array_unique($updatechecklists);
+        list($csql, $cparams) = $DB->get_in_or_equal($updatechecklists);
+        $checklists = $DB->get_records_select('checklist', 'id '.$csql, $cparams);
+        require_once($CFG->dirroot.'/mod/checklist/lib.php');
+        foreach ($checklists as $checklist) {
+            checklist_update_grades($checklist, $userid);
+        }
+    }
+
+    return $updatecount;
+}
---
+++ mod/checklist/version.php
@@ -0,0 +1,31 @@
+<?php
+
+// This file is part of the Checklist plugin for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Code fragment to define the version of checklist
+ * This fragment is called by moodle_needs_upgrading() and /admin/index.php
+ *
+ * @author  David Smith <moodle@davosmith.co.uk>
+ * @package mod/checklist
+ */
+
+$module->version  = 2012012700;  // The current module version (Date: YYYYMMDDXX)
+$module->cron     = 60;          // Period for cron to check this module (secs)
+$module->maturity = MATURITY_STABLE;
+$module->release  = '2.x (Build: 2012012700)';
+$module->requires = 2010112400;
+$module->component = 'mod_checklist';
