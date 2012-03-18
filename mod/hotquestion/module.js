50162a33810929c25e3a6e698faaa9676e671a59
Nadav Kavalerchik <nadavkav@gmail.com>
2012-03-18 20:29:37 +0200

    mod/hotquestion (update) Hebrew translation update

--- mod/hotquestion/module.js
+++ mod/hotquestion/module.js
@@ -1,90 +1,162 @@
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
 /**
- * TinyMCE helper javascript functions
+ * Handle submitting question and voting action of hotquestion
+ * using Ajax of YUI
  *
- * @package    editor_tinymce
- * @copyright  2010 Petr Skoda (skodak) info@skoda.org
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package   mod_hotquestion
+ * @copyright 2011 Sun Zhigang
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-M.editor_tinymce = M.editor_tinymce || {};
+M.mod_hotquestion = {};
 
-M.editor_tinymce.editor_options = M.editor_tinymce.options || {};
-M.editor_tinymce.filepicker_options = M.editor_tinymce.filepicker_options || {};
+M.mod_hotquestion.Y = {};
 
-M.editor_tinymce.init_editor = function(Y, editorid, options) {
-    M.editor_tinymce.editor_options[editorid] = options;
-    tinyMCE.init(options);
+M.mod_hotquestion.questionbox = {};
+M.mod_hotquestion.submitbutton = {};
+M.mod_hotquestion.refreshing = false;
 
-    var item = document.getElementById(editorid+'_filemanager');
-    if (item) {
-        item.parentNode.removeChild(item);
+M.mod_hotquestion.init = function(Y) {
+    M.mod_hotquestion.Y = Y;
+
+    // Init question box
+    M.mod_hotquestion.questionbox = Y.one('#id_question');
+    M.mod_hotquestion.questionbox.on('valueChange', M.mod_hotquestion.questionchanged);
+
+    // Init submit button
+    M.mod_hotquestion.submitbutton = Y.one('#id_submitbutton');
+    if (M.mod_hotquestion.getquestion() == '') {
+        M.mod_hotquestion.submitbutton.set('disabled', 'disabled');
     }
-};
+    Y.on("submit", M.mod_hotquestion.submit, '#mform1');
 
-M.editor_tinymce.init_filepicker = function(Y, editorid, options) {
-    M.editor_tinymce.filepicker_options[editorid] = options;
-};
+    // bind toolbar buttons
+    Y.on('click', M.mod_hotquestion.refresh, '.hotquestion_vote');
+    Y.on('click', M.mod_hotquestion.refresh, '.toolbutton');
 
-M.editor_tinymce.toggle = function(id) {
-    tinyMCE.execCommand('mceToggleEditor', false, id);
-};
+    // bind io events
+    Y.on('io:success', M.mod_hotquestion.iocomplete);
+    Y.on('io:failure', M.mod_hotquestion.iofailure);
 
-M.editor_tinymce.filepicker_callback = function(args) {
-};
+    var timer = Y.later(10000, Y, function() {
 
-M.editor_tinymce.filepicker = function(target_id, url, type, win) {
-    YUI(M.yui.loader).use('core_filepicker', function (Y) {
-        var editor_id = tinyMCE.selectedInstance.editorId;
-        if (editor_id == 'mce_fullscreen') {
-            editor_id = tinyMCE.selectedInstance.settings.elements;
+        var url = window.location;
+        var data= url.search.split('?')[1] + '&ajax=1';
+        var cfg = {
+            method : "GET",
+            data : data,
+            arguments: {
+                caller: 'refresh',
+            }
+        };
+
+        if (M.mod_hotquestion.refreshing == true) {
+            var request = M.mod_hotquestion.Y.io('view.php', cfg);
         }
-        var options = null;
-        if (type == 'media') {
-            // when mediaw button clicked
-            options = M.editor_tinymce.filepicker_options[editor_id]['media'];
-        } else if (type == 'file') {
-            // when link button clicked
-            options = M.editor_tinymce.filepicker_options[editor_id]['link'];
-        } else if (type == 'image') {
-            // when image button clicked
-            options = M.editor_tinymce.filepicker_options[editor_id]['image'];
-        } 
 
-        options.formcallback = M.editor_tinymce.filepicker_callback;
-        options.editor_target = win.document.getElementById(target_id);
+    }, '', true);
 
-        M.core_filepicker.show(Y, options);
-    });
-};
+}
 
-M.editor_tinymce.onblur_event = function(ed) {
-    //Attach event only after tinymce is intialized.
-    if (ed.onInit != undefined) {
-        var s = ed.settings;
-        //Save before event is attached, so that if this event is not generated then textarea should
-        //have loaded contents and submitting form should not throw error.
-        ed.save();
+M.mod_hotquestion.iocomplete = function(transactionid, response, arguments) {
+    var Y = M.mod_hotquestion.Y;
 
-        //Attach blur event for tinymce to save contents to textarea
-        var doc = s.content_editable ? ed.getBody() : (tinymce.isGecko ? ed.getDoc() : ed.getWin());
-        tinymce.dom.Event.add(doc, 'blur', function() {
-            //save contents to textarea before calling validation script.
-            ed.save();
-        });
+    // update questions
+    var contentdiv = Y.one('#questions_list');
+    contentdiv.set("innerHTML", response.responseText);
+
+    // clean up form if this is a submit IO
+    if (arguments.caller == 'submit') {
+        M.mod_hotquestion.questionbox.set('value', '');
+        M.mod_hotquestion.questionbox.removeAttribute('disabled');
+        M.mod_hotquestion.submitbutton.set('disabled', 'disabled');
+    }
+
+    // rebind buttons
+    Y.on('click', M.mod_hotquestion.refresh, '.hotquestion_vote');
+    Y.on('click', M.mod_hotquestion.refresh, '.toolbutton');
+}
+
+M.mod_hotquestion.iofailure = function(transactionid, response, arguments) {
+    M.mod_hotquestion.submitbutton.removeAttribute('disabled');
+    M.mod_hotquestion.questionbox.removeAttribute('disabled');
+    alert(M.str.hotquestion.connectionerror);
+}
+
+M.mod_hotquestion.refresh = function(e) {
+    e.preventDefault();
+
+    var data = e.currentTarget.get('href').split('?',2)[1];
+    data += '&ajax=1';
+    var cfg = {
+        method : "GET",
+        data : data,
+        arguments: {
+            caller: 'refresh',
+        }
     };
-};
+
+    // Switch auto refresh on/off (with 10 sec interval)
+    if (M.mod_hotquestion.refreshing == true) {
+        M.mod_hotquestion.refreshing = false;
+        M.mod_hotquestion.Y.one('#questions_list').setStyle('border','0px');
+    } else {
+        M.mod_hotquestion.refreshing = true;
+        M.mod_hotquestion.Y.one('#questions_list').setStyle('border','2px solid yellow');
+    };
+
+    //var request = M.mod_hotquestion.Y.io('view.php', cfg);
+}
+
+M.mod_hotquestion.getquestion = function() {
+    var question = M.mod_hotquestion.questionbox.get('value');
+    return YAHOO.lang.trim(question);
+}
+
+M.mod_hotquestion.questionchanged = function(e) {
+    var question = M.mod_hotquestion.getquestion();
+    var submitbutton = M.mod_hotquestion.submitbutton;
+    if (question == '') {
+        submitbutton.set('disabled', 'disabled');
+    } else {
+        submitbutton.removeAttribute('disabled');
+    }
+}
+
+M.mod_hotquestion.submit = function(e) {
+    e.preventDefault();
+
+    var question = M.mod_hotquestion.getquestion();
+    if (question == '') {
+        return; // ignore empty question
+    }
+
+    // To avoid multiple clicks and editing
+    M.mod_hotquestion.submitbutton.set('disabled', 'disabled');
+    M.mod_hotquestion.questionbox.set('disabled', 'disabled');
+
+    // Get all input components
+    var inputs = M.mod_hotquestion.Y.all('#mform1 input');
+
+    // construct post data
+    var data = '';
+    inputs.each(function(node, index, nodelist) {
+        if (node.get('type') != 'checkbox') {
+            data += node.get('name')+'='+node.get('value')+'&';
+        } else {
+            data += node.get('name')+'='+node.get('checked')+'&';
+        }
+    });
+    data += 'question='+question+'&';
+    data += 'ajax=1';
+
+    var cfg = {
+        method : "POST",
+        data : data,
+        arguments: {
+            caller: 'submit',
+        }
+    };
+    var request = M.mod_hotquestion.Y.io('view.php', cfg);
+}
+
+
