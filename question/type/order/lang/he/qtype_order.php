d9784056eaf4ef06d1d1e3282836901c448dd2ea
Nadav Kavalerchik <nadavkav@gmail.com>
2012-05-04 18:08:26 +0300

    question/type/order/lang/en/qtype_order (minor) Translation

--- question/type/order/lang/he/qtype_order.php
+++ question/type/order/lang/he/qtype_order.php
@@ -18,6 +18,7 @@ $string['horizontal'] = 'הצגה אופקית של פריטים';
 $string['itemno'] = 'פריט {$a}';
 $string['notenoughquestions'] = 'עליכם להציג, לפחות, 3 פריטים.';
 $string['order'] = 'מיון פריטים (בגרירה)';
+$string['pluginname'] = 'מיון פריטים (בגרירה)';
 $string['order_help'] = 'זו היא שאלת מיון בגרירה. את הפריטים ניתן לסדר מחדש בתצורה אופקית או אנכית.';
 $string['order_link'] = 'question/type/order';
 $string['ordersummary'] = 'זו היא שאלת מיון בגרירה. את הפריטים ניתן לסדר מחדש בתצורה אופקית או אנכית.'
