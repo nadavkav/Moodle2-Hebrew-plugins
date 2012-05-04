5977282f06699550b5cca7aed0b48cfbc63ef441
Nadav Kavalerchik <nadavkav@gmail.com>
2012-05-04 18:09:56 +0300

    mod/tab (minor) Hebrew translation

--- mod/tab/lang/he/tab.php
+++ mod/tab/lang/he/tab.php
@@ -1,29 +1,54 @@
-﻿<?php // $Id: tab.php,v 1.2 2008/07/01 09:44:05 moodler Exp $ 
-      // tab.php - created with Moodle 1.9
+<?php
 
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-$string['ajouter'] = 'הוספת דף חוצצים חדש';
-$string['addtab'] = 'הוספת חוצץ חדש';
-$string['changestyle'] = 'עריכת עיצוב';
+/**
+ * Local language pack from http://moodlemagic.info/moodle
+ *
+ * @package    mod
+ * @subpackage tab
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addtab'] = 'הוספת דף תוכן בחוצץ חדש';
+$string['ajouter'] = 'הוספת תצוגת דף תוכן בחוצץ חדש';
+$string['changestyle'] = 'עדכון עיצוב';
+$string['clicktoopen'] = 'הקליקו לצפיה';
+$string['content'] = 'תוכן';
 $string['css'] = 'עיצוב';
-$string['displaymenu'] = 'הצגת תפריט חוצצים';
-$string['displaymenuagree'] = ' סמן אם ברצונך להציג את תפריט החוצצים';
-$string['format'] = 'עיצוב חוצצים';
-$string['menucss'] = 'עריכת תפריט עיצוב';
-$string['menuname'] = 'שם התפריט';
-$string['modulename'] = 'דף חוצצים';
-$string['modulenameplural'] = 'דפי חוצצים';
-$string['moretabs'] = 'השתמש בעוד חוצצים';
+$string['displaymenu'] = 'תצוגת תפריט רכיבי חוצצים בקורס זה';
+$string['displaymenuagree'] = 'תצוגת תפריט';
+$string['externalurl'] = 'שיבוץ דף אינטרנט';
+$string['format'] = 'תצורת חוצץ';
+$string['menucss'] = 'עדכון עיצוב תפריט';
+$string['menuname'] = 'שם תפריט';
+$string['modulename'] = 'דפי תוכן בחוצצים';
+$string['modulenameplural'] = 'דפי תוכן בחוצצים';
+$string['moretabs'] = 'השתמשו בחוצצים נוספים';
 $string['name'] = 'שם';
 $string['noformating'] = 'ללא עיצוב';
-$string['order'] = 'המספר הסידורי של חוצץ זה';
-$string['pluginname'] = 'דף חוצצים';
+$string['order'] = 'מיקום בסדר תצוגת החוצצים';
 $string['pluginadministration'] = 'ניהול חוצצים';
+$string['pluginname'] = 'דפי תוכן בחוצצים';
 $string['tab'] = 'חוצץ';
+$string['tab:view'] = 'תצוגת חוצצים';
 $string['tabadministration'] = 'ניהול חוצצים';
 $string['tabcontent'] = 'תוכן החוצץ';
 $string['tabname'] = 'שם החוצץ';
-$string['taborder'] = 'מספר סידורי של החוצץ בתפריט';
-$string['updatethis'] = 'ערוך דף חוצצים';
-
-?>
+$string['taborder'] = 'סדר תצוגת רכיבי החוצצים בתפריט';
+$string['updatethis'] = 'עדכון דפי חוצצים';
