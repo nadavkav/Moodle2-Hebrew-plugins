5c372c3a09719f52c0a10ac7624458fa0a86fe71
Nadav Kavalerchik <nadavkav@gmail.com>
2012-07-24 20:12:32 +0300

    question/type/ddmatch (update)

--- question/type/ddmatch/lang/en/qtype_ddmatch.php
+++ question/type/ddmatch/lang/en/qtype_ddmatch.php
@@ -10,6 +10,7 @@
 
 $string['addingddmatch'] = 'Adding Drag-and-Drop Matching Question';
 $string['ddmatch'] = 'Drag-and-Drop Matching';
+$string['pluginname'] = 'Drag-and-Drop Matching';
 $string['ddmatch_help'] = 'Drag-and-drop matching questions require the respondent to correctly match a list of names or statements (questions) to another list of names or statements (answers).';
 $string['ddmatch_link'] = 'question/type/ddmatch';
 $string['ddmatchsummary'] = 'An extension of the matching question that allows the user to drag and drop items to match answers to subquestions.';
