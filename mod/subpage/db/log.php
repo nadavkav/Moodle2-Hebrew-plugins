14c8ae401c1b3fe63a11ae936c02c2c8db2d4b38
Nadav Kavalerchik <nadavkav@gmail.com>
2012-07-24 20:23:14 +0300

    mod/subpage (updates)

---
+++ mod/subpage/db/log.php
@@ -0,0 +1,35 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Definition of log events
+ *
+ *
+ * @package    mod_subpage
+ * @copyright  2012 The Open University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+global $DB;
+
+$logs = array(
+    array('module' => 'subpage', 'action' => 'add', 'mtable' => 'subpage', 'field' => 'name'),
+    array('module' => 'subpage', 'action' => 'update', 'mtable' => 'subpage', 'field' => 'name'),
+    array('module' => 'subpage', 'action' => 'view', 'mtable' => 'subpage', 'field' => 'name')
+);
