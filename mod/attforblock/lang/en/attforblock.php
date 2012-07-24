32f7c19d030024aa953379e16f732026dec7b25a
Nadav Kavalerchik <nadavkav@gmail.com>
2012-07-24 20:09:21 +0300

    mod/attforblock (update)

--- mod/attforblock/lang/en/attforblock.php
+++ mod/attforblock/lang/en/attforblock.php
@@ -17,6 +17,7 @@ $string['addsession'] = 'Add session';
 $string['allcourses'] = 'All courses';
 $string['all'] = 'All';
 $string['allpast'] = 'All past';
+$string['attendancedata'] = 'Attendance data';
 $string['attendanceforthecourse'] = 'Attendance for the course';
 $string['attendancegrade'] = 'Attendance grade';
 $string['attendancenotstarted'] = 'Attendance has not started yet for this course';
@@ -82,6 +83,10 @@ $string['enrolmentsuspended'] = 'Enrolment suspended';
 $string['errorgroupsnotselected'] = 'Select one or more groups';
 $string['errorinaddingsession'] = 'Error in adding session';
 $string['erroringeneratingsessions'] = 'Error in generating sessions ';
+$string['gradebookexplanation'] = 'Grade in gradebook';
+$string['gradebookexplanation_help'] = 'The Attendance module displays your current attendance grade based on the number of points you have earned to date and the number of points that could have been earned to date; it does not include class periods in the future. In the gradebook, your attendance grade is based on your current attendance percentage and the number of points that can be earned over the entire duration of the course, including future class periods. As such, your attendance grades displayed in the Attendance module and in the gradebook may not be the same number of points but they are the same percentage.
+
+For example, if you have earned 8 of 10 points to date (80% attendance) and attendance for the entire course is worth 50 points, the Attendance module will display 8/10 and the gradebook will display 40/50. You have not yet earned 40 points but 40 is the equivalent point value to your current attendance percentage of 80%. The point value you have earned in the Attendance module can never decrease, as it is based only on attendance to date; however, the attendance point value shown in the gradebook may increase or decrease depending on your future attendance, as it is based on attendance for the entire course.';
 $string['gridcolumns'] = 'Grid columns';
 $string['groupsession'] = 'Group';
 $string['hiddensessions'] = 'Hidden sessions';
@@ -154,6 +159,7 @@ $string['sortedgrid'] = 'Sorted grid';
 $string['sortedlist'] = 'Sorted list';
 $string['startofperiod'] = 'Start of period';
 $string['status'] = 'Status';
+$string['statuses'] = 'Statuses';
 $string['statusdeleted'] = 'Status deleted';
 $string['strftimedm'] = '%d.%m';
 $string['strftimedmy'] = '%d.%m.%Y';
