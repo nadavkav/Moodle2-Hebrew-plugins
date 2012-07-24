7925ffc4918a195a36a75077d01c53647f7adb56
Nadav Kavalerchik <nadavkav@gmail.com>
2012-07-24 20:19:32 +0300

    blocks/course_appointments (new)

---
+++ blocks/course_appointments/lang/en/block_course_appointments.php
@@ -0,0 +1,47 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English Language Strings for course appointments block
+ *
+ * @package block_course_appointments
+ * @author      Mark Johnson <mark.johnson@tauntons.ac.uk>
+ * @copyright   2011 Tauntons College, UK
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Appointments';
+$string['pluginnameplural'] = 'Appointments';
+$string['book'] = 'Book';
+$string['datetime'] = 'Date/Time';
+$string['entrydescription'] = 'Meeting between {$a->student} and {$a->teacher}';
+$string['entryname'] = 'Meeting with {$a}';
+$string['invaliddate'] = 'The date and time selected was invalid';
+$string['nostudent'] = 'No Student was selected';
+$string['nodate'] = 'No Date was selected';
+$string['notnotified'] = 'The student could not be notified';
+$string['notifystudent'] = 'Notify Student?';
+$string['notifysubject'] = 'Appointment to see {$a}';
+$string['notifytext'] = 'Hi {$a->student}
+{$a->teacher} has booked an appointment to see you on {$a->date} at {$a->time}.
+Please let {$a->teacher} know if you are unable to attend.';
+$string['notifysms'] = '{$a->teacher} has booked an appointment to see you on {$a->date} at
+{$a->time}. Please let them know if you are unable to attend.';
+$string['remindsms'] = 'Don\'t forget your appointment with {$a->name} at {$a->time} today.';
+$string['pastdate'] = 'The time and date selected is in the past';
+$string['studentdoesntexist'] = 'The selected student doesn\'t exist';
+$string['student'] = 'Student';
---
+++ blocks/course_appointments/process.php
@@ -0,0 +1,47 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * Processes the appointment form
+ *
+ * This page recieves the form from the course appointments block, passess the data
+ * to the validation method, and if all's well passess it on the the process method
+ * for insertion into the database.  It then returns the user to the page displaying the block.
+ * If any errors were generated during validation, processing is skipped and the errors
+ * are stored in the session for display in the block.
+ *
+ * @package block_course_appointments
+ * @author      Mark Johnson <mark.johnson@tauntons.ac.uk>
+ * @copyright   2010 Tauntons College, UK
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once('../../config.php');
+@include_once($CFG->libdir.'/sms/smslib.php');
+require_once($CFG->dirroot.'/blocks/course_appointments/course_appointments_form.php');
+require_login($SITE);
+$courseid = required_param('courseid', PARAM_INT);
+$coursecontext = get_context_instance(CONTEXT_COURSE, $courseid);
+require_capability('block/course_appointments:book', $coursecontext);
+block_load_class('course_appointments');
+$block = new block_course_appointments();
+$form = new course_appointments_form(null, array('coursecontext' => $coursecontext));
+
+if ($data = $form->get_data()) {
+    $form->process($data);
+}
+redirect($CFG->wwwroot.'/course/view.php?id='.$courseid, '', 0);
