f472a062be7d127c08916402c24f1accf9b7f6d2
Nadav Kavalerchik <nadavkav@gmail.com>
2012-08-07 19:16:53 +0300

    blocks/checklist (translation) Hebrew translation

---
+++ blocks/checklist/lang/he/block_checklist.php
@@ -0,0 +1,9 @@
+<?php
+
+$string['checklist'] = 'רשימת נושאים';
+$string['choosechecklist'] = 'בחרו רשימת נושאים';
+$string['choosegroup'] = 'בחרו קבוצה';
+$string['nochecklist'] = 'אנא בחרו פעילות רשימת-נושאים לתצוגה';
+$string['nochecklistplugin'] = 'You need to install the latest version of the checklist plugin for this block to work';
+$string['nousers'] = 'אין משתמשים';
+$string['pluginname'] = 'רשימת נושאים';
